0c02e262ceb07cd5a990e5fd4ecf30f2747b1469
oklex <alexkim.347@gmail.com>
2020-03-04 16:16:34 -0800

    initial commit

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+class App extends React.Component<{},{}> {
+  render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,6 +20,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
