b817fa99d486f2d095f2e95a3ab8083673bad4c5
oklex <alexkim.347@gmail.com>
2020-03-07 17:57:02 -0800

    added basic react-router

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Helmet from "react-helmet";
+import "./App.scss";
+import Error404 from "../Error404/Error404";
 
-class App extends React.Component<{},{}> {
+class App extends React.Component<{}, {}> {
+  showServicesList = () => {
+    return (
+      <div>
+        <h1>list here</h1>
+      </div>
+    );
+  };
+
+  showContact = () => {
+    return (
+      <div>
+        <h1>contact us</h1>
+      </div>
+    )
+  }
+
+  show404 = () => {
+    return (
+      <div>
+        <Error404 />
+      </div>
+    );
+  };
+
   render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <div className="App">
+        <Router>
+          <Helmet>
+            <meta charSet="utf-8" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0"
+            />
+          </Helmet>
+          <Switch>
+            <Route exact path="/" component={this.showServicesList} />
+            <Route exact path="/contact" component={this.showContact} />
+            <Route component={this.show404} />
+          </Switch>
+        </Router>
+      </div>
+    );
   }
 }
 
